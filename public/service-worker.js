5ae116c910829b039c2a471560bea0adad8d07de
Vince Churchill <vincechurchillankrah@gmail.com>
2024-08-29 20:30:50 +0000

    Enhancing offline functionality with service workers

---
+++ public/service-worker.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = 'actif-notes-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/icons/icons-192.png',
+  '/icons/icons-512.png',
+  // Add other static assets that need to be cached
+];
+
+// Install service worker and cache files
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Opened cache');
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Intercept network requests and serve from cache if offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Update the service worker
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
